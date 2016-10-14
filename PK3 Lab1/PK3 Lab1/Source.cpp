77522215b153d94fdb6e5aa52abbf942fd762676
Lipa <Lipa@LIPA-KOMPUTER>
2016-10-14 14:17:38 +0200

    lab1

--- PK3 Lab1/PK3 Lab1/Source.cpp
+++ PK3 Lab1/PK3 Lab1/Source.cpp
@@ -1,42 +1,3 @@
-/*Zadanie c++ (PK3 lab 1)
-Dana jest klasa :
-class Punkt
-{
-	float x
-		float y;
-public:
-	Punkt(Punkt &p);
-	Punkt(float x_ = 0, float y_ = 0);
-	dodaj(Punkt &p);//dodaje do siebie wspó³rzêdne punktu p (dodawanie wektorów w przestrzeni 2d )
-	dodaj(Punkt *p);
-	drukuj();// drukuje swoje wspó³rzêdne
-
-};
-
-1. Dla klasy zdefiniowaæ konstruktory i metody : dodaj oraz drukuj.
-Napisaæ funkcje main która sprawdza powy¿sze metody dla obiektów stworzonych statycznie i dynamicznie.
-
-Dana jest klasa :
-class Tablica
-{
-	Punkt * w;
-	int dl; // dlugoœæ Tablicy
-public:
-	Tablica(int dl = 0); // tworzy Tablice o zadanej d³ugoœci i wype³nia punktami (0,0)
-	Tablica(Tablica &w_); //inicjalizacja za pomoc¹ Tablicy w_
-	Tablica(float *x, float *y, dl); // inicjalizacja tablicy punktów tablicami x -ów i y -ów
-
-	~Tablica();
-
-	dodaj(Tablica &w_);// dodaje do siebie Tablice w_
-
-	bool porownaj(&w);// porownnaie z Tablica w_;
-};
-2. Zaimplementowaæ powy¿sze metody, konstruktory i destruktory
-Do programu ( funkcja main ) dopisaæ kod który wykorzystuje powy¿sze funkcje.
-Tablicay zainicjalizowaæ dowolnymi wartoœciami np. losowymi albo na podstawie licznika pêtli.
-Klasy , metody i program powinny byæ w odzielnych plikach (w sumie 3 pliki)*/
-
 #include <iostream>
 #include <string>
  
@@ -44,15 +5,22 @@ using namespace std;
 
 class Punkt
 {
-//public:
+	friend class Tablica;   // zaprzyjaŸnienie miedzy klasami potrzebne do porównania zawartoœci klas
 	float x;
 	float y;
+
 public:
 
-	Punkt(float x_ = 0, float y_ = 0)
-	{
+	Punkt(float x_ = 0, float y_ = 0)  //przypisanie wartoœci dla punktu, je¿eli u¿ytkownik nie poda wartoœci, zostan¹ 
+	{								   //przypisane zera
 		x = x_;
 		y = y_;
+	}
+
+	Punkt(Punkt &p2)                   // inicjalizacjia punktu innym punktem
+	{
+		x = p2.x;
+		y = p2.y;
 	}
 
 
@@ -72,50 +40,64 @@ public:
 
 class Tablica
 {
-	Punkt *w;
+	Punkt *w;  // tablica punktów
 	int dl; // dlugoœæ Tablicy
 
 public:
 
 	Tablica(int dl_ = 0) // tworzy Tablice o zadanej d³ugoœci i wype³nia punktami (0,0)
 	{
-		dl = dl_;
-		for (int i = 0 ; i < dl_ ; i++)
-		{
-			w[i] = Punkt();
-		}
+		dl = dl_;		
+		w = new Punkt[dl];  // rezerwacja pamiêci dla tablicy typu Punkt o d³ugoœci dl
 	}
 
 
-	Tablica(Tablica &tab_) //inicjalizacja za pomoc¹ Tablicy w_
+	Tablica(Tablica &tab_) //inicjalizacja za pomoc¹ Tablicy tab_
 	{
 		dl = tab_.dl;
+		w = new Punkt[dl];
 		for (int i = 0; i < tab_.dl; i++)
 		{
-			w[i] = dodaj(tab_.w[i]);
-		//	w[i].x = tab_.w[i].x;
-		//	w[i].y = tab_.w[i].y;
-
-
+			w[i].dodaj(tab_.w[i]);    // dodanie wartoœci do tablicy(wype³nionej punktami 0,0) z innej tablicy
 		}
 	}
 
-	Tablica(float x, float y, int dl_) // inicjalizacja tablicy punktów tablicami x -ów i y -ów
+	Tablica(float x_, float y_, int dl_) // inicjalizacja tablicy punktów tablicami x -ów i y -ów
 	{
-		dl = dl_;
-		
+		dl = dl_; 	
+		w = new Punkt[dl];
+		for (int i = 0; i < dl; i++)
+		{		
+			Punkt p(x_, y_);   // utworzenie zmiennej pomocniczej, która nie jest konieczna ze wzglêdu na to ¿e klasy s¹
+							   // zaprzyjaŸnione, lecz traktowa³em je tak, jakby nie by³y, gdzie tylko potrafi³em
+			w[i].dodaj(p);     // przypisanie zmiennej pomocniczej do tablicy
+		}	
 	}
 
-	~Tablica();
-
-	void dodaj(Tablica &w_) // dodaje do siebie Tablice w_
-	{
-
+	void dodaj(Tablica &tab_) // dodaje do siebie Tablice tab_
+	{		
+		for (int i = 0; i < dl; i++)
+		{			
+			w[i].dodaj(tab_.w[i]);
+		}
 	}
 
-	bool porownaj(Tablica &w_) // porownnaie z Tablica w_;
+	bool porownaj(Tablica &tab_) // porownnaie z Tablica tab_;
 	{
+		if (dl != tab_.dl)  // je¿eli ró¿ni¹ siê wielkoœci¹
+		{
+			return 0;
+		}
+		for (int i = 0; i < dl; i++)
+		{
+			if ((w[i].x != tab_.w[i].x) || (w[i].y != tab_.w[i].y)) return 0;  //je¿eli ró¿ni¹ siê zawartoœci¹
+		}
+		return 1;
+	}
 
+	~Tablica()
+	{
+		delete[]w;
 	}
 };
 
@@ -124,9 +106,18 @@ int main()
 {
 	Punkt p1(1,4);
 	Punkt p2(2,2);
+	Punkt p3(p1);
 	p1.dodaj(p2);
+	p1.dodaj(p3);
 	p1.drukuj();
 	Tablica tab1(5);
-	Tablica tab2(tab1);
+	Tablica tab2(1,3,5);
+	Tablica tab3(tab2);
+	tab3.dodaj(tab2);
+	if (tab3.porownaj(tab1) == 1)
+	{
+		cout << "tablice sa takie same" << endl;
+	}
+	else cout << "tablice sie roznia" << endl;
 	return 0;
 }
